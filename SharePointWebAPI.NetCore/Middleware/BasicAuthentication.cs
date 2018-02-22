78369abc968c00527265331d78849672729372bd
KHTeh <funcoolgeek@gmail.com>
2018-02-22 13:56:50 +0800

    Rename MIddlewares to Middleware

--- SharePointWebAPI.NetCore/Middleware/BasicAuthentication.cs
+++ SharePointWebAPI.NetCore/Middleware/BasicAuthentication.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebAPI.NetCore.MIddlewares
+namespace WebAPI.NetCore.Middleware
 {
     public class BasicAuthentication
     {
